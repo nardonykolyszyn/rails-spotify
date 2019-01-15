897771d5c06975773df85fb69b4a97ea3644c506
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2019-01-15 15:37:00 -0500

    precompile production assets

--- config/application.rb
+++ config/application.rb
@@ -1,24 +1,23 @@
 require_relative 'boot'
-require 'rspotify'
-require "rails"
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "action_cable/engine"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'active_storage/engine'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+require 'action_cable/engine'
+require 'sprockets/railtie'
+require 'rspotify'
+require 'rails'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+Dotenv::Railtie.load if %w[development test].include? ENV['RAILS_ENV']
 
-Dotenv::Railtie.load
-RSpotify::authenticate(ENV.fetch('CLIENT_ID'), ENV.fetch('CLIENT_SECRET'))
+RSpotify.authenticate ENV.fetch('CLIENT_ID'), ENV.fetch('CLIENT_SECRET')
 
 module RailsSpotify
   class Application < Rails::Application
