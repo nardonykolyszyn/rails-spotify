6bb239b361cb22da6da789e6fb171a2fae61ef2d
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2019-01-16 00:40:47 -0500

    render playlists

--- app/controllers/spotify/categories_controller.rb
+++ app/controllers/spotify/categories_controller.rb
@@ -4,6 +4,7 @@ module Spotify
   class CategoriesController < SpotifyController
     def search
       @categories = params[:name].present? ? Category.list : Category.find(params[:name])
+      @categories = @categories.playlists if params[:playlists].present? && @categories.respond_to?(:playlists)
       render json: { categories: @categories }, status: :ok
     end
   end
