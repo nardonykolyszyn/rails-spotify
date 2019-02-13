676d5e5cb860e923fc3753680eafb89f750622ac
LivinnX <livinnx@iMac-de-LivinnX.local>
2019-02-13 15:47:09 -0500

    set up root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root "home#index"
   namespace :spotify do
     get 'artists', to: 'artists#search'
     get 'playlists', to: 'playlists#search'
